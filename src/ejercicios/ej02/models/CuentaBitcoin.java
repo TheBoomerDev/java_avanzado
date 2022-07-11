a0d8f18a57e96c208a05064a2844326dff11dce4
Jorge Parada <jorge.parada@ctbarbanza.com>
2022-07-11 19:31:57 +0200

    -

--- src/ejercicios/ej02/models/CuentaBitcoin.java
+++ src/ejercicios/ej02/models/CuentaBitcoin.java
@@ -1,5 +1,7 @@
 package ejercicios.ej02.models;
 
+import java.util.List;
+
 import ejercicios.ej01.interfaces.CuentaBanco;
 import ejercicios.ej01.interfaces.LlegadaLimite;
 import ejercicios.ej02.common.Tools;
@@ -8,30 +10,55 @@ import ejercicios.ej02.interfaces.ObservadorCuenta;
 public class CuentaBitcoin extends AbsCuentaBanco{
 	
 	private float saldo = 0f;
-	private ObservadorCuenta listener;
+	private List<ObservadorCuenta> listeners = new ArrayList<ObservadorCuenta>();
 	
 	public CuentaBitcoin(ObservadorCuenta listener) throws Exception {
 		this.numeroCuenta = Tools.getRandomHexString(16);
 		if (listener == null) {
 			throw new Exception("Necesitamos el Limite");
 		} 
-		this.listener = listener; 
+		this.listeners.add(listener);
 	}
 
 	@Override
 	public void meter(int cantidad) {
 		this.saldo = this.saldo + cantidad; 
+		if (this.listeners.size() > 0) {
+			this.listeners.forEach(listener->{
+				try {
+					listener.opCorrecta(this);
+				} catch (Exception e) {}
+			});
+		}
 	}
 
 	@Override
 	public boolean sacar(int cantidad) {
 		
 		if ((cantidad <= 0) || (this.saldo <= 0) || (this.saldo < cantidad) ) {
+			
+			if (this.listeners.size() > 0) {
+				this.listeners.forEach(listener->{
+					try {
+						listener.noHayFondos(this);
+					} catch (Exception e) {}
+				});
+			}
+			
 			return false;
 		} 
 		
 		float amount = ((float)cantidad)/100f;
 		this.saldo = this.saldo - amount;
+		
+		if (this.listeners.size() > 0) {
+			this.listeners.forEach(listener->{
+				try {
+					listener.opCorrecta(this);
+				} catch (Exception e) {}
+			});
+		}
+		
 		return true;
 	}
 
