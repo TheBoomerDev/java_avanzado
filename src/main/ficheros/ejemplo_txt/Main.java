bc45b02228040658d2b7f8c95fa66e0da623ec13
Jorge Parada <jorge.parada@ctbarbanza.com>
2022-07-12 17:43:13 +0200

    File Read + Create

---
+++ src/main/ficheros/ejemplo_txt/Main.java
@@ -0,0 +1,39 @@
+package main.ficheros.ejemplo_txt;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+ 
+		System.out.println("Lectura de Fichero");
+		
+		try {
+		      File myObj = new File("filename.txt");
+		      
+		      if (myObj.createNewFile()) {
+		          System.out.println("File created: " + myObj.getName());
+		        } else {
+		          System.out.println("File already exists.");
+		        }
+		      
+		      
+		      Scanner myReader = new Scanner(myObj);
+		      while (myReader.hasNextLine()) { // Hasta el EOF - End Of File
+		        String data = myReader.nextLine();
+		        System.out.println(data);
+		      }
+		      myReader.close();
+		    } catch (FileNotFoundException e) {
+		      System.out.println("An error occurred.");
+		      e.printStackTrace();
+		    } catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
+
+}
